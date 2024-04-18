debe9ba61fae1d4c800d0e7c6954872032a22475
scott-lydon <scottlydon18@gmail.com>
2024-04-18 16:41:59 -0700

    Make the defaultJson public

--- Sources/StrongContractResponse/All/HTTPHeaders.swift
+++ Sources/StrongContractResponse/All/HTTPHeaders.swift
@@ -9,7 +9,7 @@ import NIO
 import NIOHTTP1
 
 extension HTTPHeaders {
-    static var defaultJson: HTTPHeaders {
+    public static var defaultJson: HTTPHeaders {
         var headers = HTTPHeaders()
         headers.add(name: "Content-Type", value: "application/json")
         return headers
