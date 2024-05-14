786a424574e426d3981829942793fcb57ba1c827
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-13 21:45:42 -0300

    Add why reguster downloader breaks contract explanation

--- Sources/StrongContractResponse/All/StrongContract.handle.swift
+++ Sources/StrongContractResponse/All/StrongContract.handle.swift
@@ -9,9 +9,9 @@
 import Vapor
 
 public extension StrongContractClient.Request {
-
-    typealias Handler = (Payload, Vapor.Request) async throws -> ResponseAdaptor
-
+    
+    typealias PayloadToResponse = (Payload, Vapor.Request) async throws -> ResponseAdaptor
+    
     /// This method registers routes, and exposes a callback for
     ///  the call site to process the request and return a response
     /// - Parameters:
@@ -21,19 +21,19 @@ public extension StrongContractClient.Request {
     func register(
         app: any RoutesBuilder,
         verbose: Bool = false,
-        handler: @escaping Handler
+        payloadToResponse: @escaping PayloadToResponse
     ) {
         // Split the path by '/' to get individual components
         // Convert string path segments to PathComponent
         let pathComponents = path.split(separator: "/").map(String.init).map(PathComponent.init)
-
+        
         if verbose {
             print(pathComponents)
         }
-
+        
         switch method {
         case .get, .head:
-
+            
             // Register a route to handle HEAD requests.
             // In Vapor, HEAD requests are handled by GET route handlers without
             // sending the body in the response.
@@ -42,36 +42,40 @@ public extension StrongContractClient.Request {
             // but the response body will not be sent to the client.
             app.get(pathComponents) {
                 if verbose { print("We received: \($0)") }
-                return try await handler($0.decodedObject(), $0).vaporResponse
+                return try await payloadToResponse($0.decodedObject(), $0).vaporResponse
             }
         case .post:
             app.post(pathComponents) {
                 if verbose { print("We received: \($0)") }
-                return try await handler($0.decodedObject(), $0).vaporResponse
+                return try await payloadToResponse($0.decodedObject(), $0).vaporResponse
             }
         case .put:
             app.put(pathComponents) {
                 if verbose { print("We received: \($0)") }
-                return try await handler($0.decodedObject(), $0).vaporResponse
+                return try await payloadToResponse($0.decodedObject(), $0).vaporResponse
             }
         case .delete:
             app.delete(pathComponents) {
                 if verbose { print("We received: \($0)") }
-                return try await handler($0.decodedObject(), $0).vaporResponse
+                return try await payloadToResponse($0.decodedObject(), $0).vaporResponse
             }
         case .patch:
             app.patch(pathComponents) {
                 if verbose { print("We received: \($0)") }
-                return try await handler($0.decodedObject(), $0).vaporResponse
+                return try await payloadToResponse($0.decodedObject(), $0).vaporResponse
             }
         }
     }
+}
 
-    typealias Downloader = (Payload?, Vapor.Request) async throws -> Vapor.Response
+public extension StrongContractClient.Request where Response == Data {
+
+    typealias PayloadToData = (Payload?, Vapor.Request) async throws -> Vapor.Response
+
     func register(
         app: any RoutesBuilder,
         verbose: Bool = false,
-        downloader: @escaping Downloader
+        downloader: @escaping PayloadToData
     ) {
         let pathComponents = path.split(separator: "/").map(String.init).map(PathComponent.init)
         if verbose {
@@ -106,5 +110,4 @@ public extension StrongContractClient.Request {
         }
     }
 }
-
 #endif
