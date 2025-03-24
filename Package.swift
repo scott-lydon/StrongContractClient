833e8ee99229bde9b72caca852bbfa36c4988474
Scott Lydon <scottlydon18@gmail.com>
2025-03-24 13:54:19 -0700

    More dependencies getting conditional imports encryptDecryptkey

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Existing dependencies
         .package(url: "https://github.com/ElevatedUnderdogs/Callable.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/scott-lydon/EncryptDecryptKey.git", from: "1.0.0")
+        .package(url: "https://github.com/scott-lydon/EncryptDecryptKey.git", from: "1.0.5")
     ],
     targets: [
         .target(
