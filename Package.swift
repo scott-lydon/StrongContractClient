dc9911fc8655b3cd39acf8736c8efabec53e4740
Scott Lydon <scottlydon18@gmail.com>
2026-01-13 22:46:45 -0800

    update callable dependency.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Existing dependencies
-        .package(url: "https://github.com/ElevatedUnderdogs/Callable.git", .upToNextMajor(from: "3.0.1")),
+        .package(url: "https://github.com/ElevatedUnderdogs/Callable.git", .upToNextMajor(from: "3.0.2")),
         .package(url: "https://github.com/scott-lydon/EncryptDecryptKey.git", from: "1.1.2")
     ],
     targets: [
